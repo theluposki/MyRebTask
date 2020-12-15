99059dcd6896c11b3ef6b7f72d1353aa0a61a150
theluposki <theluposki@gmail.com>
2020-12-15 16:35:43 -0300

    port heroku

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-app.listen(process.env.server_port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running in > <http://localhost:3000>`)
 })
